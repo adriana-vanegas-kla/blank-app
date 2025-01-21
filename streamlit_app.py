f5cc3b8f96991323cc49c0e84406ab91e0b1c26e
adriana-vanegas-kla <134088360+adriana-vanegas-kla@users.noreply.github.com>
2025-01-21 18:08:42 +0000

    Updated Title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Existing Customer's Gross Margin")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
